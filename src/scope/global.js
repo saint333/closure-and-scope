c9047fed6db0745275f3c521a95a8a10778ce0a2
David Israel <israeldavi0904@gmail.com>
2022-02-16 11:39:35 -0500

    scope global

---
+++ src/scope/global.js
@@ -0,0 +1,12 @@
+var hola = "hola"
+let mundo = "mundo"
+const holamundo = "hola mundo"
+/* las variables declaradas con var se les puede cambiar su valor mientras con let y const no se puede */
+
+const otraFuncion = () => {
+    console.log(hola);
+    console.log(mundo);
+    console.log(holamundo);
+}
+
+otraFuncion()
