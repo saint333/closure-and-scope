cea4a0ba9ff9ae352e58f944d1b86acb5466c2ca
David Israel <israeldavi0904@gmail.com>
2022-03-02 09:17:17 -0500

    scope de bloque

---
+++ src/scope/block.js
@@ -0,0 +1,26 @@
+const fruits = () => {
+    if (true) {
+        var fruits1 = "apple"
+        let fruits2 = "banana"
+        const fruits3 = "kiwi"
+    }
+    console.log(fruits1);
+    console.log(fruits1);
+    console.log(fruits1);
+}
+
+fruits()
+
+let x = 1
+{
+    let x = 2
+    console.log(x);
+}
+console.log(x);
+
+var x = 1
+{
+    var x = 2
+    console.log(x);
+}
+console.log(x);
